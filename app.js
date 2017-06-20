02aa6fddf556d97933142d61a0dbf0eaf3a1af0a
kvin1087 <kaustubh87@gmail.com>
2017-06-20 16:17:07 -0500

    Added new routes

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req,res){
     res.send('Hello World');
+});
+
+
+
+app.get('/cities', function(req,res){
+    var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+    res.json(cities);
 });
 
 app.listen(3000, function(){
     console.log('Server running at 3000');
 });
+
 
 module.exports = app;
