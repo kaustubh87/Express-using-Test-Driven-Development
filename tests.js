539c02d5d9e9d439c6753875cdf51c33c5e690d9
kvin1087 <kaustubh87@gmail.com>
2017-06-20 21:02:31 -0500

    Added new city form addition test

--- tests.js
+++ tests.js
@@ -2,49 +2,67 @@ var request = require('supertest');
 var app = require('./app');
 
 describe('Requests to the root path', function () {
-    it('Returns a 200 status code', function (done) {
 
+    it('Returns a 200 status code', function (done) {
         request(app)
             .get('/')
             .expect(200, done);
-               
-        });
+    });
 
-    it('Returns in a HTML format' ,function(done){
+    it('Returns a HTML format', function (done) {
         request(app)
             .get('/')
-            .expect('Content-Type', '/html/', done);
+            .expect('Content-Type', /html/, done);
     });
 
-    it('Returns an index file with Cities' ,function(done){
+    it('Returns an index file with Cities', function (done) {
         request(app)
             .get('/')
-            .expect('/cities/i', done);
+            .expect(/cities/i, done);
     });
 
+});
 
+describe('Listing Cities on /cities', function () {
 
+    it('Returns 200 status code', function (done) {
+        request(app)
+            .get('/cities')
+            .expect(200, done);
     });
 
-    describe('Listing Cities on /cities', function () {
+    it('Returns JSON format', function (done) {
 
-        it('Returns 200 status code', function (done) {
+        request(app)
+            .get('/cities')
+            .expect('Content-Type', /json/, done);
+    });
+
+    it('Returns initial cities', function (done) {
+        request(app)
+            .get('/cities')
+            .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+    });
+});
+
+
+describe('Creating new cities', function(){
+
+        it('Returns a 201 status code', function(done){
+
             request(app)
-                .get('/cities')
-                .expect(200, done);
+            .post('/cities')
+            .send('name=Springfield&description=where+is+simpsons')
+            .expect(201, done);
         });
 
-        it('Returns JSON format', function (done) {
+
+        it('Return the city name', function(done){
 
             request(app)
-                .get('/cities')
-                .expect('Content-Type', /json/, done);
-        });
-
-         it('Returns initial cities', function(done){
-            request(app)
-                .get('/cities')
-                .expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+                .post('/cities/')
+                .send('name=Springfield&description=where+is+simpsons')
+                .expect(/springfield/i, done);
         });
 });
 
