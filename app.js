4ef14734feacd8b6c8f446326d63f8bd15e2b75d
kvin1087 <kaustubh87@gmail.com>
2017-06-20 21:36:28 -0500

    Added new post route

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+var urlencoded = bodyParser.urlencoded({ extended: false});
 
 app.get('/', function(req,res){
     res.send('Hello World');
 });
 
-
+ var cities = {
+        'Lotopia': 'description1',
+        'Caspiana': 'description2', 
+        'Indigo': 'description3' 
+       };
 
 app.get('/cities', function(req,res){
-    var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-    res.json(cities);
+    
+   
+
+    res.json(Object.keys(cities));
+
+});
+
+app.post('/cities', urlencoded, function(req,res){
+    var newCity = req.body;
+    cities[newCity.name] = newCity.description;
+    res.status(201).json(newCity.name);
 });
 
 app.listen(3000, function(){
     console.log('Server running at 3000');
 });
+
+
 
 
 module.exports = app;
